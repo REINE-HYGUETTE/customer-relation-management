a99477b90679256db37de87f1b9d6975755f9f36
REINE-HYGUETTE <reinehyguette@gmail.com>
2024-11-07 17:58:55 +0200

    Delete src directory

--- src/main/java/net/enjoy/springboot/CustomerRelationManagement/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package net.enjoy.springboot.CustomerRelationManagement.repository;
-
-import net.enjoy.springboot.CustomerRelationManagement.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
